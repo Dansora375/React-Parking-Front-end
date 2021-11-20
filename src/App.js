383378c706492037e494490fa699acf1bd13c206
crramirezma <crramirezma@unal.edu.co>
2021-11-20 11:15:33 -0500

    Prueba de funcionamiento git en vscode

--- src/App.js
+++ src/App.js
@@ -3,39 +3,75 @@ import './App.css';
 // import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
 // import {Hogares} from './views/Hogares/Hogares.jsx'
 
+// hooks react redux
+import {useDispatch, useSelector} from 'react-redux'
+
+// importamos la acción
+import { loguinWithEmailAction } from './redux/Ducks/authDuck'
+
+import { Hogares } from './views/Hogares/Hogares';
+
+  import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink
+  } from "react-router-dom";
+
 function App() {
+
+  // const dispatch = useDispatch()
+
+  // const authentication = useSelector(store=>store.authentication.userData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        {/* <BrowserRouter>
-          <Link to='/hogares' 
-            className="App-link" 
-            target="_blank" 
-            rel="noopener noreferrer">
-              Hogares
-          </Link>
-        </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p> 
+    //     {/* <button onClick={()=> dispatch(loguinWithEmailAction("Hey"))}>
+    //       {authentication.message}
+    //     </button> */}
+    //     {/* <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a> */}
+    //     {/* <BrowserRouter>
+    //       <Link to='/hogares' 
+    //         className="App-link" 
+    //         target="_blank" 
+    //         rel="noopener noreferrer">
+    //           Hogares
+    //       </Link>
+    //     </BrowserRouter>
 
-        <Routes>
-          <Route path='/hogares'>
-            <Hogares/>
+    //     <Routes>
+    //       <Route path='/hogares'>
+    //         <Hogares/>
+    //       </Route>
+    //     </Routes> */}
+
+    //   </header>
+    // </div>
+
+    <Router>
+      <Link to='/hogares'>
+        hogares
+      </Link>
+
+      <Switch>
+          <Route path="/hogares" exact>
+            <Hogares />
           </Route>
-        </Routes> */}
-
-      </header>
-    </div>
+          
+        </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
+import Login from './views/authentication/login';
 import {Hogares} from './views/Hogares/Hogares';
 
+
+import { Provider } from 'react-redux'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import { createStore } from 'redux';
+
+import generateStore from './redux/store'
+
+const store = generateStore()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Hogares />
-  </React.StrictMode>,
+
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/signin">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+    
+  </Provider>,
   document.getElementById('root')
 );

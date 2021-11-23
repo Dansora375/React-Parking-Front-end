313921bcdfd7a51c184f645ae5458259e01a9297
Dansora375 <danisora375@gmail.com>
2021-11-22 23:36:35 -0500

    themes listos

---
+++ src/components/common/SearchAndAdd/SearchAndAdd.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Grid, Box, Fab, Menu, TextField} from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import './SearchAndAdd.css'
+
+const IconColor= "#14FFEC"
+
+function SearchAndAdd() {
+  return (
+    <>
+      <Grid container 
+      justifyContent="flex-end"
+      alignItems="flex-end"
+      spacing={0} >
+        <Grid item xs={8.8}>
+          <Fab aria-label="add" size="small" id="boton"
+                 >
+                <AddIcon   />
+            </Fab>
+            
+        </Grid>
+
+          
+        <Grid item xs={2}>
+          <Box className='input-container'> 
+            <TextField id="standard-basic" label="Filtrar..." variant="standard" 
+            size="large"/>
+          </Box> 
+            
+          
+           
+              
+            </Grid>
+             <Grid item xs={1}>
+              <IconButton aria-label="search"> 
+              <SearchIcon fontSize="large" sx={{ color: IconColor}}/>  
+              </IconButton>
+            </Grid>
+
+       
+          
+    </Grid>
+    
+    </>
+  )
+}
+
+export default SearchAndAdd
---
+++ src/themes/themes.js
@@ -0,0 +1,28 @@
+
+const darkTheme ={ 
+
+  main: '#14FFEC',
+  secondary: '#0D7377',
+  tertiary : '#BCFFFA',
+  other:'#e5e8ec',
+  background:'#323232'
+  
+}
+
+// const lightTheme ={ 
+
+//   main: '#14FFEC',
+//   secondary: '#0D7377',
+//   tertiary : '#BCFFFA',
+//   other:'#e5e8ec',
+//   background:'#323232'
+  
+// }
+  
+const themes ={
+  dark:darkTheme,
+  // light:lightTheme
+  
+}
+
+export default themes

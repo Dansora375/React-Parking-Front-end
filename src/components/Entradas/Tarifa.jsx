eccc0fa2a2eed7ef0450aac54e26e723780d3d69
ramtako8922 <rmc892020@gmail.com>
2021-11-30 09:08:22 -0500

    FALTA CUADRAR COLOR DEL VALOR DE LA TARIFA

--- src/components/Entradas/Tarifa.jsx
+++ src/components/Entradas/Tarifa.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Modal1 from '../Hogares/Modal1'
 import Grid from '@mui/material/Grid';
-import {TextField}from '@mui/material/TextField';
+import TextField from '@mui/material/TextField';
 import {useState} from 'react'
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button';
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Button'
 
 export default function Tarifa() {
     const [estadoModal1, cambiarEstadoModal1] = useState(false);
@@ -20,7 +21,7 @@ export default function Tarifa() {
     <Modal1 estado = {estadoModal1} cambiarEstado = {cambiarEstadoModal1} >
         
                 
-                <Grid item xs={12} sx={{textAlign:'center',background:'#14FFEC',color: '#323232', borderRadius:'5px'}}>
+                <Grid item xs={12} sx={{textAlign:'center',background:'#BCFFFA',color: '#323232', borderRadius:'5px'}}>
                   
                 <h2 sx={{textAlign:'cnter'}}>Tarifa Parqueo </h2>
                 </Grid>
@@ -54,12 +55,12 @@ export default function Tarifa() {
                     <Grid item xs={3} sx={{textAlign: 'center'}}>
                         <b>20:52 23/10/2021</b>
                     </Grid>
-                    <Grid item xs={12} sx={{textAlign: 'left'}}>
-                    <Button variant="contained">Calcular tarifa</Button>
+                    <Grid item xs={12} sx={{textAlign: 'center'}}>
+                    <Button variant="contained" sx={{background:'#323232', color: '#BCFFFA'}}>Calcular tarifa</Button>
                         
                     </Grid>
                     <Grid item xs={12} sx={{textAlign: 'center'}}>
-                        <TextField sx={{backgroundColor:'#14FFEC'}}>$ 3000</TextField>
+                        <TextField sx={{backgroundColor:'#BCFFFA', color:'#323232', float:'center'}} value="$ 3000"></TextField>
                     </Grid>
                     </Grid>
                  </Modal1>
